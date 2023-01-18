e0625d0f9510602dc9195581a85627f92605e7f1
t-takahashi <tkhstol.929@gmail.com>
2023-01-19 08:16:15 +0900

    update controller

--- src/main/java/io/github/tttol/mrls/controller/MrListController.java
+++ src/main/java/io/github/tttol/mrls/controller/MrListController.java
@@ -3,6 +3,10 @@ package io.github.tttol.mrls.controller;
 import io.github.tttol.mrls.service.MergeRequestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +20,13 @@ public class MrListController {
     private final MergeRequestService mergeRequestService;
 
     @GetMapping
-    public String list(Model model) {
+    public String list(Model model,
+                       @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient oAuth2AuthorizedClient,
+                       @AuthenticationPrincipal OAuth2User oAuth2User) {
         model.addAttribute("mrInfoFormList", mergeRequestService.get());
+        model.addAttribute("userName", oAuth2User.getName());
+        model.addAttribute("clientName", oAuth2AuthorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oAuth2User.getAttributes());
         return "list";
     }
 }
