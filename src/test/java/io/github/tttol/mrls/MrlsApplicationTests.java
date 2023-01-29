87085ccb065c31f6e1d72e1e58ebe2edeea4d956
t-takahashi <tkhstol.929@gmail.com>
2023-01-30 08:44:31 +0900

    update README,md

--- src/test/java/io/github/tttol/mrls/MrlsApplicationTests.java
+++ src/test/java/io/github/tttol/mrls/MrlsApplicationTests.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"GITLAB_PROJECT_ID=dummy",
-    "GITLAB_ACCESS_TOKEN=dummy"})
+        "GITLAB_ACCESS_TOKEN=dummy"})
 class MrlsApplicationTests {
 
   @Test
   void contextLoads() {
+
   }
 
 }
