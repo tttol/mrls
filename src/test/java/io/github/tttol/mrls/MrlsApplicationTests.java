612944bc5fd9d38c6fa511b7e5a8c6cee6baa9e5
t-takahashi <tkhstol.929@gmail.com>
2023-01-29 23:24:15 +0900

    MrlsApplicationTests 修正

--- src/test/java/io/github/tttol/mrls/MrlsApplicationTests.java
+++ src/test/java/io/github/tttol/mrls/MrlsApplicationTests.java
@@ -3,11 +3,12 @@ package io.github.tttol.mrls;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"GITLAB_PROJECT_ID=dummy",
+    "GITLAB_ACCESS_TOKEN=dummy"})
 class MrlsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
