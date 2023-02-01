6d4b03f73fb53c203cf6620a74c175eb1b617c8d
TOL <tkhstol.929@gmail.com>
2023-02-02 08:20:39 +0900

    state=openのMRに絞る、LocalDateTime -> OffsetDateTimeにリファクタ (#16)

--- src/main/java/io/github/tttol/mrls/service/MergeRequestService.java
+++ src/main/java/io/github/tttol/mrls/service/MergeRequestService.java
@@ -5,75 +5,69 @@ import io.github.tttol.mrls.external.GitLabApiExecutor;
 import io.github.tttol.mrls.form.MrDetailForm;
 import io.github.tttol.mrls.form.MrInfoForm;
 import io.github.tttol.mrls.form.UserForm;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.ZoneOffset;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MergeRequestService {
 
-  private final GitLabApiExecutor gitLabApiExecutor;
+    private final GitLabApiExecutor gitLabApiExecutor;
 
-  public List<MrInfoForm> get(String accessToken) {
-    var mergeRequestInfoDtos = executeGitLabApi(accessToken);
-    return mergeRequestInfoDtos.stream()
-        .collect(Collectors.groupingBy(
-                e -> Objects.isNull(e.getAssignee()) ?
-                    e.getAuthor().getId() : e.getAssignee().getId()
-            )
-        )
-        .values().stream().map(this::generateForm)
-        .sorted(Comparator.comparing(e -> e.assignee().id()))
-        .toList();
-  }
-
-  private List<GitLabMergeRequestApiResponseDto> executeGitLabApi(String accessToken) {
-    return gitLabApiExecutor.getMergeRequests(accessToken);
-  }
-
-  private MrInfoForm generateForm(List<GitLabMergeRequestApiResponseDto> responseDtos) {
-    var responseDto = responseDtos.stream().findAny().orElseThrow();
-    var assigneeForm = Optional.ofNullable(responseDto.getAssignee())
-        .map(assignee -> new UserForm(
-                assignee.getId(),
-                assignee.getUsername(),
-                assignee.getName(),
-                assignee.getState(),
-                assignee.getAvatarUrl(),
-                assignee.getWebUrl()
-            )
-        ).orElse(UserForm.empty());
-    var linkForms = responseDtos.stream().map(e ->
-        new MrDetailForm(
-            e.getTitle(),
-            e.getWebUrl(),
-            Optional.ofNullable(e.getAuthor())
-                .map(assignee -> new UserForm(
-                        assignee.getId(),
-                        assignee.getUsername(),
-                        assignee.getName(),
-                        assignee.getState(),
-                        assignee.getAvatarUrl(),
-                        assignee.getWebUrl()
-                    )
+    public List<MrInfoForm> get(final String accessToken) {
+        final var mergeRequestInfoDtos = executeGitLabApi(accessToken);
+        return mergeRequestInfoDtos.stream()
+                .collect(Collectors.groupingBy(
+                                e -> Objects.isNull(e.getAssignee()) ?
+                                        e.getAuthor().getId() : e.getAssignee().getId()
+                        )
                 )
-                .orElse(UserForm.empty()),
-            toJst(e.getCreatedAt()),
-            toJst(e.getUpdatedAt())
-        )).toList();
-    return new MrInfoForm(assigneeForm, linkForms, linkForms.size());
-  }
+                .values().stream().map(this::generateForm)
+                .sorted(Comparator.comparing(e -> e.assignee().id()))
+                .toList();
+    }
 
-  private LocalDateTime toJst(LocalDateTime utc) {
-    return ZonedDateTime.of(utc, ZoneId.of("UTC"))
-        .withZoneSameInstant(ZoneId.of("Asia/Tokyo")).toLocalDateTime();
-  }
+    private List<GitLabMergeRequestApiResponseDto> executeGitLabApi(final String accessToken) {
+        return gitLabApiExecutor.getMergeRequests(accessToken);
+    }
+
+    private MrInfoForm generateForm(final List<GitLabMergeRequestApiResponseDto> responseDtos) {
+        final var responseDto = responseDtos.stream().findAny().orElseThrow();
+        final var assigneeForm = Optional.ofNullable(responseDto.getAssignee())
+                .map(assignee -> new UserForm(
+                                assignee.getId(),
+                                assignee.getUsername(),
+                                assignee.getName(),
+                                assignee.getState(),
+                                assignee.getAvatarUrl(),
+                                assignee.getWebUrl()
+                        )
+                ).orElse(UserForm.empty());
+        final var linkForms = responseDtos.stream().map(e ->
+                new MrDetailForm(
+                        e.getTitle(),
+                        e.getWebUrl(),
+                        Optional.ofNullable(e.getAuthor())
+                                .map(assignee -> new UserForm(
+                                                assignee.getId(),
+                                                assignee.getUsername(),
+                                                assignee.getName(),
+                                                assignee.getState(),
+                                                assignee.getAvatarUrl(),
+                                                assignee.getWebUrl()
+                                        )
+                                )
+                                .orElse(UserForm.empty()),
+                        e.getCreatedAt().withOffsetSameInstant(ZoneOffset.of("+09:00")),
+                        e.getUpdatedAt().withOffsetSameInstant(ZoneOffset.of("+09:00"))
+                )).toList();
+        return new MrInfoForm(assigneeForm, linkForms, linkForms.size());
+    }
 }
--- src/test/java/io/github/tttol/mrls/service/MergeRequestServiceTest.java
+++ src/test/java/io/github/tttol/mrls/service/MergeRequestServiceTest.java
@@ -1,201 +1,199 @@
 package io.github.tttol.mrls.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
-
 import io.github.tttol.mrls.dto.GitLabMergeRequestApiResponseDto;
 import io.github.tttol.mrls.dto.UserDto;
 import io.github.tttol.mrls.external.GitLabApiExecutor;
 import io.github.tttol.mrls.form.MrDetailForm;
 import io.github.tttol.mrls.form.MrInfoForm;
 import io.github.tttol.mrls.form.UserForm;
-import java.time.LocalDateTime;
-import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+
 public class MergeRequestServiceTest {
 
-  private AutoCloseable closeable;
+    private AutoCloseable closeable;
 
-  @BeforeEach
-  void setUp() {
-    closeable = MockitoAnnotations.openMocks(this);
-  }
-
-  @AfterEach
-  void close() throws Exception {
-    closeable.close();
-  }
-
-  @InjectMocks
-  private MergeRequestService mergeRequestService;
-
-  @Mock
-  private GitLabApiExecutor gitLabApiExecutor;
-
-  @Nested
-  class Get {
-
-    @Test
-    @DisplayName("GitLabからMRを取得しdto->formの変換ができること")
-    void formTest() {
-      var assignee = UserDto.builder()
-          .id(1)
-          .username("assignee_username1")
-          .name("assignee_name1")
-          .state("active")
-          .build();
-      var mergeRequestInfoDtos = List.of(
-          GitLabMergeRequestApiResponseDto.builder()
-              .assignee(assignee)
-              .author(UserDto.builder()
-                  .id(11)
-                  .username("author_username11")
-                  .name("author_name11")
-                  .state("active")
-                  .build())
-              .webUrl("url1")
-              .title("title1")
-              .createdAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0))
-              .updatedAt(LocalDateTime.of(2000, 1, 1, 0, 0, 0))
-              .build(),
-          GitLabMergeRequestApiResponseDto.builder()
-              .assignee(assignee)
-              .author(UserDto.builder()
-                  .id(11)
-                  .username("author_username12")
-                  .name("author_name12")
-                  .state("active")
-                  .build())
-              .webUrl("url2")
-              .title("title2")
-              .createdAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0))
-              .updatedAt(LocalDateTime.of(2000, 1, 1, 0, 0, 0))
-              .build(),
-          GitLabMergeRequestApiResponseDto.builder()
-              .assignee(assignee)
-              .author(UserDto.builder()
-                  .id(13)
-                  .username("author_username13")
-                  .name("author_name13")
-                  .state("active")
-                  .build())
-              .webUrl("url3")
-              .title("title3")
-              .createdAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0))
-              .updatedAt(LocalDateTime.of(2000, 1, 1, 0, 0, 0))
-              .build()
-      );
-      doReturn(mergeRequestInfoDtos).when(gitLabApiExecutor).getMergeRequests(anyString());
-      var expected = List.of(new MrInfoForm(
-          new UserForm(1, "assignee_username1", "assignee_name1", "active", null, null),
-          List.of(
-              new MrDetailForm(
-                  "title1",
-                  "url1",
-                  new UserForm(
-                      11,
-                      "author_username11",
-                      "author_name11",
-                      "active",
-                      null,
-                      null
-                  ),
-                  LocalDateTime.of(1970, 1, 1, 9, 0, 0),
-                  LocalDateTime.of(2000, 1, 1, 9, 0, 0)
-              ),
-              new MrDetailForm(
-                  "title2",
-                  "url2",
-                  new UserForm(
-                      11,
-                      "author_username12",
-                      "author_name12",
-                      "active",
-                      null,
-                      null
-                  ),
-                  LocalDateTime.of(1970, 1, 1, 9, 0, 0),
-                  LocalDateTime.of(2000, 1, 1, 9, 0, 0)
-              ),
-              new MrDetailForm(
-                  "title3",
-                  "url3",
-                  new UserForm(
-                      13,
-                      "author_username13",
-                      "author_name13",
-                      "active",
-                      null,
-                      null
-                  ),
-                  LocalDateTime.of(1970, 1, 1, 9, 0, 0),
-                  LocalDateTime.of(2000, 1, 1, 9, 0, 0)
-
-              )
-          ),
-          3
-      ));
-
-      var actual = mergeRequestService.get(anyString());
-
-      assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    @DisplayName("MRのAssigneeが未指定の場合")
-    void noAssigneeTest() {
-      var mergeRequestInfoDtos = List.of(
-          GitLabMergeRequestApiResponseDto.builder()
-              .assignee(null)
-              .author(UserDto.builder()
-                  .id(11)
-                  .username("author_username11")
-                  .name("author_name11")
-                  .state("active")
-                  .build())
-              .webUrl("url1")
-              .title("title1")
-              .createdAt(LocalDateTime.of(1970, 1, 1, 0, 0, 0))
-              .updatedAt(LocalDateTime.of(2000, 1, 1, 0, 0, 0))
-              .build()
-      );
-      doReturn(mergeRequestInfoDtos).when(gitLabApiExecutor).getMergeRequests(anyString());
-      var expected = List.of(new MrInfoForm(
-          UserForm.empty(),
-          List.of(
-              new MrDetailForm(
-                  "title1",
-                  "url1",
-                  new UserForm(11, "author_username11", "author_name11", "active", null, null),
-                  LocalDateTime.of(1970, 1, 1, 9, 0, 0),
-                  LocalDateTime.of(2000, 1, 1, 9, 0, 0)
-              )
-          ),
-          1
-      ));
-
-      var actual = mergeRequestService.get(anyString());
-
-      assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    @AfterEach
+    void close() throws Exception {
+        closeable.close();
     }
 
-    @Test
-    @DisplayName("MR0件の場合")
-    void noMrTest() {
-      doReturn(List.of()).when(gitLabApiExecutor).getMergeRequests(anyString());
-      var expected = List.of();
-      var actual = mergeRequestService.get(anyString());
+    @InjectMocks
+    private MergeRequestService mergeRequestService;
 
-      assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+    @Mock
+    private GitLabApiExecutor gitLabApiExecutor;
+
+    @Nested
+    class Get {
+
+        @Test
+        @DisplayName("GitLabからMRを取得しdto->formの変換ができること")
+        void formTest() {
+            final var assignee = UserDto.builder()
+                    .id(1)
+                    .username("assignee_username1")
+                    .name("assignee_name1")
+                    .state("active")
+                    .build();
+            final var mergeRequestInfoDtos = List.of(
+                    GitLabMergeRequestApiResponseDto.builder()
+                            .assignee(assignee)
+                            .author(UserDto.builder()
+                                    .id(11)
+                                    .username("author_username11")
+                                    .name("author_name11")
+                                    .state("active")
+                                    .build())
+                            .webUrl("url1")
+                            .title("title1")
+                            .createdAt(OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .updatedAt(OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .build(),
+                    GitLabMergeRequestApiResponseDto.builder()
+                            .assignee(assignee)
+                            .author(UserDto.builder()
+                                    .id(11)
+                                    .username("author_username12")
+                                    .name("author_name12")
+                                    .state("active")
+                                    .build())
+                            .webUrl("url2")
+                            .title("title2")
+                            .createdAt(OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .updatedAt(OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .build(),
+                    GitLabMergeRequestApiResponseDto.builder()
+                            .assignee(assignee)
+                            .author(UserDto.builder()
+                                    .id(13)
+                                    .username("author_username13")
+                                    .name("author_name13")
+                                    .state("active")
+                                    .build())
+                            .webUrl("url3")
+                            .title("title3")
+                            .createdAt(OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .updatedAt(OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .build()
+            );
+            doReturn(mergeRequestInfoDtos).when(gitLabApiExecutor).getMergeRequests(anyString());
+            final var expected = List.of(new MrInfoForm(
+                    new UserForm(1, "assignee_username1", "assignee_name1", "active", null, null),
+                    List.of(
+                            new MrDetailForm(
+                                    "title1",
+                                    "url1",
+                                    new UserForm(
+                                            11,
+                                            "author_username11",
+                                            "author_name11",
+                                            "active",
+                                            null,
+                                            null
+                                    ),
+                                    OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")),
+                                    OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00"))
+                            ),
+                            new MrDetailForm(
+                                    "title2",
+                                    "url2",
+                                    new UserForm(
+                                            11,
+                                            "author_username12",
+                                            "author_name12",
+                                            "active",
+                                            null,
+                                            null
+                                    ),
+                                    OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")),
+                                    OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00"))
+                            ),
+                            new MrDetailForm(
+                                    "title3",
+                                    "url3",
+                                    new UserForm(
+                                            13,
+                                            "author_username13",
+                                            "author_name13",
+                                            "active",
+                                            null,
+                                            null
+                                    ),
+                                    OffsetDateTime.of(1970, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00")),
+                                    OffsetDateTime.of(2000, 1, 1, 0, 0, 0, 0, ZoneOffset.of("+09:00"))
+
+                            )
+                    ),
+                    3
+            ));
+
+            final var actual = mergeRequestService.get(anyString());
+
+            assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("MRのAssigneeが未指定の場合")
+        void noAssigneeTest() {
+            final var mergeRequestInfoDtos = List.of(
+                    GitLabMergeRequestApiResponseDto.builder()
+                            .assignee(null)
+                            .author(UserDto.builder()
+                                    .id(11)
+                                    .username("author_username11")
+                                    .name("author_name11")
+                                    .state("active")
+                                    .build())
+                            .webUrl("url1")
+                            .title("title1")
+                            .createdAt(OffsetDateTime.of(1970, 1, 1, 9, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .updatedAt(OffsetDateTime.of(2000, 1, 1, 9, 0, 0, 0, ZoneOffset.of("+09:00")))
+                            .build()
+            );
+            doReturn(mergeRequestInfoDtos).when(gitLabApiExecutor).getMergeRequests(anyString());
+            final var expected = List.of(new MrInfoForm(
+                    UserForm.empty(),
+                    List.of(
+                            new MrDetailForm(
+                                    "title1",
+                                    "url1",
+                                    new UserForm(11, "author_username11", "author_name11", "active", null, null),
+                                    OffsetDateTime.of(1970, 1, 1, 9, 0, 0, 0, ZoneOffset.of("+09:00")),
+                                    OffsetDateTime.of(2000, 1, 1, 9, 0, 0, 0, ZoneOffset.of("+09:00"))
+                            )
+                    ),
+                    1
+            ));
+
+            final var actual = mergeRequestService.get(anyString());
+
+            assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        }
+
+        @Test
+        @DisplayName("MR0件の場合")
+        void noMrTest() {
+            doReturn(List.of()).when(gitLabApiExecutor).getMergeRequests(anyString());
+            final var expected = List.of();
+            final var actual = mergeRequestService.get(anyString());
+
+            assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
+        }
     }
-  }
 }
