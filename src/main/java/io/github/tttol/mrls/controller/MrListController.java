d916d40665b819822eedb3e79412d0612515e085
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-01-29 08:50:35 +0900

    Refactor MrListController and list.html
    This commit refactors the MrListController and list.html files. It adds two new model attributes in the MrListController and modifies the th:classappend attribute in list.html to apply different CSS classes based on the createdAt date.

--- src/main/java/io/github/tttol/mrls/controller/MrListController.java
+++ src/main/java/io/github/tttol/mrls/controller/MrListController.java
@@ -1,18 +1,19 @@
 package io.github.tttol.mrls.controller;
 
-import io.github.tttol.mrls.service.MergeRequestService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.time.OffsetDateTime;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.tttol.mrls.service.MergeRequestService;
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequestMapping("/list")
 @RequiredArgsConstructor
-@Slf4j
 public class MrListController {
 
     private final MergeRequestService mergeRequestService;
@@ -20,6 +21,8 @@ public class MrListController {
     @GetMapping("/pat") // Project Access Toekn
     public String pat(final Model model) {
         model.addAttribute("mrInfoFormList", mergeRequestService.get(StringUtils.EMPTY));
+        model.addAttribute("oneWeekAgo", OffsetDateTime.now().minusDays(7));
+        model.addAttribute("twoWeeksAgo", OffsetDateTime.now().minusDays(14));
         return "list";
     }
 
