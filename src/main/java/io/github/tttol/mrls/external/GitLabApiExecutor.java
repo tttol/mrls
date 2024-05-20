cfebac7ea4f89ac2a75d1efa50e4577316db587b
t-takahashi <tttol.t4k.b1z@gmail.com>
2024-05-20 23:04:26 +0900

    add proxy & update gradle, spring boot, jdk

--- src/main/java/io/github/tttol/mrls/external/GitLabApiExecutor.java
+++ src/main/java/io/github/tttol/mrls/external/GitLabApiExecutor.java
@@ -1,26 +1,27 @@
 package io.github.tttol.mrls.external;
 
-import io.github.tttol.mrls.dto.GitLabMergeRequestApiResponseDto;
-import io.github.tttol.mrls.exception.GitLabApiException;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
+
+import io.github.tttol.mrls.dto.GitLabMergeRequestApiResponseDto;
+import io.github.tttol.mrls.exception.GitLabApiException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class GitLabApiExecutor {
 
-  private final RestTemplate restTemplate;
+  private final RestClient restClient;
 
   @Value("${app.gitlab.api.endpoint}")
   private String endpoint;
@@ -28,21 +29,21 @@ public class GitLabApiExecutor {
   private String projectAccessToken;
 
   public List<GitLabMergeRequestApiResponseDto> getMergeRequests(String accessToken) {
-    // TODO 例外処理
     try {
-      var requestEntity = RequestEntity
-          .get(new URI(endpoint))
+      var token = StringUtils.isNotBlank(accessToken) ? accessToken : projectAccessToken;
+      var responseEntity = restClient.get()
+          .uri(new URI(endpoint))
           .accept(MediaType.APPLICATION_JSON)
-          .header("Authorization",
-              "Bearer %s".formatted(StringUtils.isNotBlank(accessToken) ?
-                  accessToken : projectAccessToken))
-          .build();
-      log.info("api url -> {}", requestEntity.getUrl());
-      var responseEntity = restTemplate.exchange(requestEntity,
-          GitLabMergeRequestApiResponseDto[].class);
-      log.debug("status code -> {}", responseEntity.getStatusCode());
-      var body = responseEntity.getBody();
-      return Objects.isNull(body) ? List.of() : Arrays.asList(body);
+          .header("Authorization", "Bearer %s".formatted(token))
+          .retrieve()
+          .toEntity(GitLabMergeRequestApiResponseDto[].class);
+      
+      if (responseEntity != null) {
+        log.info("status code -> {}", responseEntity.getStatusCode());
+        var body = responseEntity.getBody();
+        return Objects.isNull(body) ? List.of() : Arrays.asList(body);
+      }
+      return List.of();
     } catch (Exception e) {
       throw new GitLabApiException(e);
     }
