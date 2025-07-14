61ceaa462ee28d347863787e0a4462279bb0c7e6
Toru Takahashi <tkhstol.929@gmail.com>
2025-07-14 21:34:27 +0900

    SpringBoot & Gradle VUP (#40)

--- src/test/java/io/github/tttol/mrls/controller/RequestListControllerTest.java
+++ src/test/java/io/github/tttol/mrls/controller/RequestListControllerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -35,10 +35,10 @@ public class RequestListControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
     private MockMvc mockMvc;
-    @MockBean
+    @MockitoBean
     @Qualifier("gitlabRequestService")
     private IRequestService requestService;
-    @MockBean
+    @MockitoBean
     @Qualifier("gitlabPresenter")
     private IPresenter presenter;
 
